fcf1a26367d20b092269007e871746c0a3bca805
Lukimiaa <lukmanbaihaqqi15@gmail.com>
2026-05-31 20:59:50 +0700

    Initial commit

---
+++ BE-KawanKAsir/src/main/kotlin/com/lukimia/bekawankasir/BeKawanKAsirApplication.kt
@@ -0,0 +1,11 @@
+package com.lukimia.bekawankasir
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeKawanKAsirApplication
+
+fun main(args: Array<String>) {
+    runApplication<BeKawanKAsirApplication>(*args)
+}
---
+++ BE-KawanKAsir/src/test/kotlin/com/lukimia/bekawankasir/BeKawanKAsirApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lukimia.bekawankasir
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeKawanKAsirApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
